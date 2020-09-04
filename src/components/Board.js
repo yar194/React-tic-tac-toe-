48d095ac7a5cd5961b6170fc4a2c91782029f483
YarZarTun <yarzartunhl@gmail.com>
2020-09-04 17:42:15 +0630

    initial commit

---
+++ src/components/Board.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Square from './Square';
+
+export default function Board(props){
+    const renderSquare =(i) => {
+        return (
+            <Square 
+            value={props.squares[i]} 
+            onClick={()=>props.onClick(i)}
+        ></Square>
+        );
+    };
+    return(
+        <div>
+            <div className="row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
